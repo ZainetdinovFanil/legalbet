a5de99705475d5bf8f66c0bf8cb50a29e3c192b1
Fanil <zainetdinov102@mail.ru>
2024-10-01 19:27:48 +0500

    fix area animation

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
     const cardBody = document.querySelector('.card__body');
     const lungeIcon = document.querySelector('.lunge__icon');
+    const lungeBlock = document.querySelector('.lunge__block');
     const cardText = document.querySelector('.card__text');
 
-    lungeIcon.addEventListener('click', () => {
+    lungeBlock.addEventListener('click', () => {
         if (cardBody.classList.contains('expanded')) {
             cardBody.classList.remove('expanded');
             lungeIcon.classList.remove('rotated');
